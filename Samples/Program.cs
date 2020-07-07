b7588341f991e70b7fc51c3f70c6b5194ab33024
Devwarlt <devwarlt@loesoft.org>
2020-07-07 10:55:04 -0300

    Import console application template

---
+++ Samples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Packt.Csharp.Linq.Samples
+{
+    public sealed class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
